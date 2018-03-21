705c61bc27d80c930081c4b444f73de02a25f7e4
Fuller Le <fullernle@mail.fresnostate.edu>
2018-03-21 16:27:45 -0700

    Milestone presentation

---
+++ MidTerm/include/enemy.h
@@ -0,0 +1,43 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include<Model.h>
+#include<gl/gl.h>
+#include<textureLoader.h>
+#include <string.h>
+#include <iostream>
+#include <player.h>
+
+using namespace std;
+
+typedef struct
+{
+   float x;
+   float y;
+   float z;
+}vec2;
+
+class enemy:public Model{
+    public:
+        enemy();
+        virtual ~enemy();
+
+        float scalesize[3]={1.0,1.0,1.0};
+        vec2 verticies[4];
+
+        float runSpeed;
+        float xpos;
+        float ypos;
+
+        void drawEnemy();
+        void deleteEnemy();
+        void enemyInit();
+        void enemyScroll(player* ply);
+        bool isCollided(player*);
+
+    protected:
+
+    private:
+};
+
+#endif // ENEMY_H
